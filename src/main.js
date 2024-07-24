5194e41c7ee4d32dd5f3b052002f7d18db6382ae
koleso8 <dev.koleso@gmail.com>
2024-07-24 14:33:10 +0300

    add

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './js/API_reviews';
+import './js/about-me';
+import './js/benefits';
+import './js/covers';
+import './js/faq';
+import './js/header';
+import './js/hero';
+import './js/projects';
+import './js/reviews';
+import './js/work-together';
+import './js/modal';
