f4b6ed39664e27c90920aa630f0d3d681e1a3705
koleso8 <168295877+koleso8@users.noreply.github.com>
2024-07-25 01:15:06 +0000

    Deploying to gh-pages from @ koleso8/js2team_proj@3a8a7bc9e4722d16e32c1f20afa30c3390bf435d 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{A as r,a as c}from"./assets/vendor-c626b9c5.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();r.create({baseURL:"https://portfolio-js.b.goit.study/api"});const l={listEl:document.querySelector(".faq-list")},d=[{title:"What programming languages are most often used in your project?",text:"In the field of web development, there are various technologies and programming languages. Typically, this includes a combination of frontend (client side) and backend (server side) languages."},{title:"What are the deadlines for the project?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"What payment methods do you accept?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"How can I contact you?",text:"In the field of web development, there are various technologies and programming languages. Typically, this includes a combination of frontend (client side) and backend (server side) languages."},{title:"Do you provide advice or support?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"What does the process of developing a software product look like from idea to implementation?",text:"Payment through credit and debit cards such as Visa, MasterCard, specialized electronic payment systems such as PayPal, as well as payments in cryptocurrencies such as Bitcoin, Ethereum and others."}];function u(o){const s=o.title,i=o.text;return`
+import{A as n,a as c,b as l}from"./assets/vendor-c4f2e476.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();n.create({baseURL:"https://portfolio-js.b.goit.study/api"});const d={listEl:document.querySelector(".faq-list")},u=[{title:"What programming languages are most often used in your project?",text:"In the field of web development, there are various technologies and programming languages. Typically, this includes a combination of frontend (client side) and backend (server side) languages."},{title:"What are the deadlines for the project?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"What payment methods do you accept?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"How can I contact you?",text:"In the field of web development, there are various technologies and programming languages. Typically, this includes a combination of frontend (client side) and backend (server side) languages."},{title:"Do you provide advice or support?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"What does the process of developing a software product look like from idea to implementation?",text:"Payment through credit and debit cards such as Visa, MasterCard, specialized electronic payment systems such as PayPal, as well as payments in cryptocurrencies such as Bitcoin, Ethereum and others."}];function p(o){const s=o.title,a=o.text;return`
     <li class="ac" id="ac">
     <h3 class="ac-header">
     <button type="button" class="ac-trigger" id="ac-header">
@@ -12,9 +12,25 @@ import{A as r,a as c}from"./assets/vendor-c626b9c5.js";(function(){const s=docum
     </h3>
     <div class="ac-panel">
     <p class="ac-text" id="ac-text">
-    ${i}
+    ${a}
     </p>
     </div>
     </li>
-    `}function p(o){return o.map(u).join("")}function f(o){const s=p(o);l.listEl.insertAdjacentHTML("afterbegin",s)}f(d);new c(".accordion-container",{});
+    `}function h(o){return o.map(p).join("")}function f(o){const s=h(o);d.listEl.insertAdjacentHTML("afterbegin",s)}f(u);new c(".accordion-container",{});l.create(`
+     <div class="container">
+     <div class="modal">
+    <h3 class="title-modal">Thank you for your interest in cooperation!</h3>
+    <p class="text-modal">
+      The manager will contact you shortly to discuss further details and
+      opportunities for cooperation. Please stay in touch.
+    </p>
+    <a href="" class="close"
+      ><svg class="icon" width="22" height="22">
+        <use href="svg/icons.svg#close"></use>
+      </svg>
+    </a>
+  </div>
+</div>
+
+`,{onShow:o=>{o.element().querySelector("a").onclick=o.close}});
 //# sourceMappingURL=commonHelpers.js.map
