8b26483bc25d46dc8511527df5d8166cd648abfc
koleso8 <168295877+koleso8@users.noreply.github.com>
2024-07-25 00:18:12 +0000

    Deploying to gh-pages from @ koleso8/js2team_proj@f9fdbba3ca097807e0cf8e0313d8be0a0a03fc90 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,20 @@
-import{A as n}from"./assets/vendor-8f98089f.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();new n(".accordion-container",{});
+import{A as r,a as c}from"./assets/vendor-c626b9c5.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();r.create({baseURL:"https://portfolio-js.b.goit.study/api"});const l={listEl:document.querySelector(".faq-list")},d=[{title:"What programming languages are most often used in your project?",text:"In the field of web development, there are various technologies and programming languages. Typically, this includes a combination of frontend (client side) and backend (server side) languages."},{title:"What are the deadlines for the project?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"What payment methods do you accept?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"How can I contact you?",text:"In the field of web development, there are various technologies and programming languages. Typically, this includes a combination of frontend (client side) and backend (server side) languages."},{title:"Do you provide advice or support?",text:"The terms of project implementation depend to a large extent on a number of factors, such as the scope of the project, its complexity, and the availability of resources. Determining exact deadlines is a difficult task, and it usually occurs during the planning phase of the project."},{title:"What does the process of developing a software product look like from idea to implementation?",text:"Payment through credit and debit cards such as Visa, MasterCard, specialized electronic payment systems such as PayPal, as well as payments in cryptocurrencies such as Bitcoin, Ethereum and others."}];function u(o){const s=o.title,i=o.text;return`
+    <li class="ac" id="ac">
+    <h3 class="ac-header">
+    <button type="button" class="ac-trigger" id="ac-header">
+    ${s}
+    <span class="circle">
+    <svg class="icon" width="12" height="8">
+    <use href="svg/icons.svg#arrow-down"></use>
+    </svg>
+    </span>
+    </button>
+    </h3>
+    <div class="ac-panel">
+    <p class="ac-text" id="ac-text">
+    ${i}
+    </p>
+    </div>
+    </li>
+    `}function p(o){return o.map(u).join("")}function f(o){const s=p(o);l.listEl.insertAdjacentHTML("afterbegin",s)}f(d);new c(".accordion-container",{});
 //# sourceMappingURL=commonHelpers.js.map
