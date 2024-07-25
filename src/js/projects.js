2771d4a30bafa104200f0a0fc06dfcea87d97918
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-07-25 21:03:20 +0300

    Update projects.js

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,8 +1,8 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 import svg from '../images/svg/icons.svg';
+
 import image1_mob1x from '../images/projects/image_1_mob@1x.webp';
-console.log(image1_mob1x);
 import image1_mob2x from '../images/projects/image_1_mob@2x.webp';
 import image1_tab1x from '../images/projects/image_1_tab_desk@1x.webp';
 import image1_tab2x from '../images/projects/image_1_tab_desk@2x.webp';
@@ -54,28 +54,28 @@ const projectsList = [
   },
 ];
 
-console.log(projectsList.imgMob1);
-
 const markupEl = document.querySelector('.projects-cards');
+const navBtnPrev = document.querySelector('.projects-navigation-btn');
+const navBtnNext = document.querySelector('');
 
-function cartMarkupTemplate(arr) {
+function cardMarkupTemplate(arr) {
   return arr
     .map(item => {
-      return `<li class="projects-card">
+      return `<li class="projects-card swiper-slide">
           <div class="projects-card-desc">
             <ul class="projects-card-skills">
-              <li><p class="projects-card-skill">${projectsList.skill1}</p></li>
-              <li><p class="projects-card-skill">${projectsList.skill2}</p></li>
-              <li><p class="projects-card-skill">${projectsList.skill3}</p></li>
-              <li><p class="projects-card-skill">${projectsList.skill4}</p></li>
+              <li><p class="projects-card-skill">${item.skill1}</p></li>
+              <li><p class="projects-card-skill">${item.skill2}</p></li>
+              <li><p class="projects-card-skill">${item.skill3}</p></li>
+              <li><p class="projects-card-skill">${item.skill4}</p></li>
             </ul>
             <p class="projects-card-title">
-              ${projectsList.title}
+              ${item.title}
             </p>
             <a
               class="projects-card-link"
               href="https://github.com/koleso8/js2team_proj"
-              >${projectsList.link}</a
+              >${item.link}</a
             >
           </div>
           <div class="projects-card-image">
@@ -83,29 +83,29 @@ function cartMarkupTemplate(arr) {
             <source
                  media="(min-width: 1440px)"
                 srcset="
-                ${projectsList.imgTab1} 1x,
-                ${projectsList.imgTab2} 2x
+                ${item.imgTab1} 1x,
+                ${item.imgTab2} 2x
                 "
                 type="image/webp"
             />
             <source
                 media="(min-width: 768px)"
                 srcset="
-                ${projectsList.imgTab1} 1x,
-                ${projectsList.imgTab2} 2x
+                ${item.imgTab1} 1x,
+                ${item.imgTab2} 2x
                 "
                 type="image/webp"
             />
             <source
                 media="(max-width: 767px)"
                 srcset="
-                ${projectsList.imgMob1} 1x,
-                ${projectsList.imgMob2} 2x
+                ${item.imgMob1} 1x,
+                ${item.imgMob2} 2x
                 "
                 type="image/webp"
             />
             <img
-                src="${projectsList.imgMob1}"
+                src="${item.imgMob1}"
                 alt="project"
                 width="311"
                 height="195"
@@ -119,8 +119,17 @@ function cartMarkupTemplate(arr) {
 }
 
 function renderProjectCart(arr) {
-  const markup = cartMarkupTemplate(arr);
+  const markup = cardMarkupTemplate(arr);
   markupEl.innerHTML = markup;
 }
 
 renderProjectCart(projectsList);
+
+const swiper = new Swiper('.projects-wrapper', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
