6644da8560f819a94f4bc2b1acc8162d2c430f75
Julia <jpkoord@gmail.com>
2024-07-26 17:19:09 +0300

    projectcover

--- src/js/covers.js
+++ src/js/covers.js
@@ -17,3 +17,38 @@ let options = {
 
 let observer = new IntersectionObserver(onEntry, options);
 observer.observe(coversSection);
+
+
+/*  <picture>
+            <source
+                 media="(min-width: 1440px)"
+                srcset="
+                ${item.imgTab1} 1x,
+                ${item.imgTab2} 2x
+                "
+                type="image/webp"
+            />
+            <source
+                media="(min-width: 768px)"
+                srcset="
+                ${item.imgTab1} 1x,
+                ${item.imgTab2} 2x
+                "
+                type="image/webp"
+            />
+            <source
+                media="(max-width: 767px)"
+                srcset="
+                ${item.imgMob1} 1x,
+                ${item.imgMob2} 2x
+                "
+                type="image/webp"
+            />
+            <img
+                src="${item.imgMob1}"
+                alt="project"
+                width="311"
+                height="195"
+                class="projects-card-img"
+            />
+            </picture> */
