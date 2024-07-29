b46c1b5497587a939f37cd7c99a06b788dc0812e
koleso8 <dev.koleso@gmail.com>
2024-07-29 11:46:33 +0300

    add

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,44 +1,41 @@
-import { createRequest } from "./API_reviews";
-import {success} from "./modal"
+import { createRequest } from './API_reviews';
+import { success } from './modal';
 
 const formEl = document.querySelector('.form');
 const inputEl = document.querySelector('.comments');
 const firstInput = document.querySelector('.email');
 
+formEl.addEventListener('submit', async event => {
+  event.preventDefault();
 
-formEl.addEventListener('submit', async (event) => {
-    event.preventDefault();
+  const emailInput = formEl.elements.email.value.trim();
+  const commentsInput = formEl.elements.comments.value.trim();
 
-    const emailInput = formEl.elements.email.value.trim();
-    const commentsInput = formEl.elements.comments.value.trim();
-
-    const request = {
+  const request = {
     email: emailInput,
-    comment: commentsInput 
+    comment: commentsInput,
+  };
 
-    }
+  await createRequest(request);
+  success();
 
-    await createRequest(request);
-    success();
-
-    formEl.reset();
-
-})
+  formEl.reset();
+});
 
 inputEl.addEventListener('input', () => {
-    const comment = inputEl.value.slice(0, 150).replace("...", "");
-    if (comment.length >= 150) {
-        inputEl.value = comment + "...";
-     }
-})
+  const comment = inputEl.value.slice(0, 150).replace('...', '');
+  if (comment.length >= 150) {
+    inputEl.value = comment + '...';
+  }
+});
 
 firstInput.addEventListener('input', () => {
-    const email = firstInput.value;
-    if (email.validity.valid) {
-        email.classList.add('valid');
-        email.classList.remove('invalid');
-    } else {
-        email.classList.add('invalid');
-        email.classList.remove('valid');
-    }
-})
+  const email = firstInput;
+  if (email.validity.valid) {
+    email.classList.add('valid');
+    email.classList.remove('invalid');
+  } else {
+    email.classList.add('invalid');
+    email.classList.remove('valid');
+  }
+});
